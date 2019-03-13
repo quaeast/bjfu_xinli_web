3e853b301a4f5d8fbb35a5f639b82eba498066a5
ZiDong Fang <424044876@qq.com>
2019-03-13 16:33:58 +0800

    add a server.js for publicip

---
+++ server_public.js
@@ -0,0 +1,12 @@
+const express = require('express'); //express框架模块
+const path = require('path'); //系统路径模块
+const app = express();
+
+const hostName = '192.168.31.128'; //ip
+const port = 8080; //端口
+
+app.use(express.static(path.join(__dirname, '.'))); //指定静态文件目录
+
+app.listen(port, function() {
+    console.log(`服务器运行在http://${hostName}:${port}`);
+});
