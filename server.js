39d1d1289d822efbee01ea90c216982181d30e95
ZiDong Fang <424044876@qq.com>
2019-05-04 00:59:42 +0800

    change server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path'); //系统路径模块
 const app = express();
 
 const hostName = '192.168.31.128'; //ip
-const port = 8080; //端口
+const port = 80; //端口
 
 app.use(express.static(path.join(__dirname, '.'))); //指定静态文件目录
 
-app.listen(port, hostName, function() {
+app.listen(port, function() {
     console.log(`服务器运行在http://${hostName}:${port}`);
 });
