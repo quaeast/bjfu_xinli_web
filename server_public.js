f76c55c63a185f08ad41eebe7286bfc721c8d025
ZiDong Fang <424044876@qq.com>
2019-06-24 23:49:09 +0800

    add logo and adjast position of related links

--- server_public.js
+++ server_public.js
@@ -2,12 +2,12 @@
 const path = require('path'); //系统路径模块
 const app = express();
 
-const hostName = '192.168.31.128'; //ip
+const hostName = '149.129.37.165'; //ip
 const port = 8080; //端口
 
 app.use(express.static(path.join(__dirname, '.'))); //指定静态文件目录
 
-app.listen(port, function() {
+app.listen(port, hostName, function() {
   console.log(`服务器运行在http://${hostName}:${port}`);
 });
 const express = require('express'); //express框架模块
